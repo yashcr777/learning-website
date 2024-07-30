f3da13d51e70d51f1c2b606802de21398b7ad0fb
yashcr777 <yashagarwalnobody@gmail.com>
2024-07-30 22:17:58 +0530

    first committtrttrrr

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,40 @@
+import {db} from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async({
+    params
+}:{
+    params:{courseId:string}
+}) => {
+    const {userId}=auth();
+    
+    if(!userId){
+        return redirect("/");
+    }
+    const course =await db.course.findUnique({
+        where:{
+            id:params.courseId
+        }
+    });
+    if(!course){
+        return redirect("/");
+    }
+
+    const requiredFields=[
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.categoryId,
+    ]
+
+
+    return ( 
+        <div>
+            Course Id:{params.courseId}
+        </div>
+     );
+}
+ 
+export default CourseIdPage;
