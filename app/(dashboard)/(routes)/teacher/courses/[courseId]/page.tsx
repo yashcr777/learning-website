f5e6821cca8a9cce768183bc775219638b344728
yashcr777 <yashagarwalnobody@gmail.com>
2024-07-31 00:26:58 +0530

    first committtrttrrrrwuyg

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -28,11 +28,32 @@ const CourseIdPage = async({
         course.price,
         course.categoryId,
     ]
+    const totalFields=requiredFields.length;
+    const completedFields=requiredFields.filter(Boolean).length;
 
+    const completionText=`(${completedFields}/${totalFields})`
 
     return ( 
-        <div>
-            Course Id:{params.courseId}
+        <div className="p-6">
+            <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-y-2">
+                    <h1 className="text-2xl font-medium">
+                        Course setup
+                    </h1>
+                    <span className="text-sm text-slate-700">
+                        Complete all fields {completionText}
+                    </span>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <h2 className="text-xl">
+                            Customize your course
+                        </h2>
+                    </div>
+                </div>
+            </div>
         </div>
      );
 }
